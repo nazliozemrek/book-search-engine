4609b3ee83cf785c87c038c328a807eec7b831a8
nazliozemrek <77180888+nazliozemrek@users.noreply.github.com>
2021-07-29 02:31:01 -0500

    showing pages

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,17 @@ const {AuthenticationError} = require('apollo-server-express');
 const{ signToken } = require('../utils/auth');
 const resolvers = {
   Query:{
-    me:async (parent,args) => {
-      
-    }
+    me:async (parent,args,context) => {
+      if(context.user){
+        const userData = await User.findOne({})
+        .select('-__v -password')
+        .populate('books')
+
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in')
+
+    },
 
   },
   Mutation:{
@@ -27,6 +35,28 @@ const resolvers = {
 
       const token=signToken(user);
       return{token,user};
+    },
+    saveBook: async(parent,args,context)=> {
+      if(context.user){
+        const updatedUser = await User.findByIdAndUpdate(
+          {_id:context.user._id},
+          {$addToSet:{savedBooks:args.input}},
+          {new:true}
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async(parent,args,context)=> {
+      if(context.user){
+        const updatedUser = await User.findByIdAndUpdate(
+          {_id:context.user_id},
+          {$pull:{savedBooks:{bookId:args.bookId}}},
+          {new:true}
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('You need to be logged in!');
     }
   }
 }
